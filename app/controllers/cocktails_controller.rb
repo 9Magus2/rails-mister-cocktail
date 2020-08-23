9aecad7cacb46777319a3c0454a259bed52ed697
Agata Wojcik <agataizabelawojcik@gmail.com>
2020-08-23 14:49:58 +0200

    CocktailsController

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -11,13 +11,17 @@ class CocktailsController < ApplicationController
 
   def new
     @cocktail = Cocktail.new 
+   
+
   end
 
   def create
     @cocktail = Cocktail.new(cocktail_params)
-    @cocktail.save
-
-    redirect_to cocktails_path(@cocktail)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   private
