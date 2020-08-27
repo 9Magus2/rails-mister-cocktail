8146ba6b9a04c254a7114398a00fc2cdb4d3b3e3
Agata Wojcik <agataizabelawojcik@gmail.com>
2020-08-27 18:46:22 +0200

    test

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -28,7 +28,7 @@ class DosesController < ApplicationController
   end
 
   def dose_params
-    params.require(:dose).permit( :description, :cocktail_id, :ingredient_id )
+    params.require(:dose).permit( :description, :cocktail_id, :ingredient_id)
   end
 end
  
